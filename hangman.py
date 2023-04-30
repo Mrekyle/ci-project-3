85e9f7db3180056ff5c0f0ecd6aa5d1eed7efe03
Mrekyle <>
2023-04-30 16:55:13 +0000

    Add hangman file containing words and state of hangman for game

---
+++ hangman.py
@@ -0,0 +1,76 @@
+# Words for the hangman game based on the difficulty level selected
+
+beginner = [
+    'Ant',
+    'Apple',
+    'Tree',
+    'Bee',
+    'Doll',
+    'Sun',
+    'Moon',
+    'Earth',
+    'Deer',
+    'Chair',
+    'Horse',
+    'Family',
+    'Friend'
+]
+
+novice = [
+    'Attack',
+    'Aftermath',
+    'Math',
+    'Attend',
+    'Supply',
+    'Pocket',
+    'Protect',
+    'Group',
+    'Fruit',
+    'Fuel',
+    'Melon',
+    'Guide',
+    'Empty',
+    'Discover',
+    'Cable',
+    'Helmet',
+    'Reward',
+    'Chef'
+]
+
+advanced = [
+    'Encyclopedia',
+    'Extravaganza',
+    'Entrepreneur',
+    'Accomplished',
+    'Phenomenally',
+    'Sophisticated',
+    'International',
+    'Remuneration',
+    'Satisfactory',
+    'Stereoscopic',
+    'Masked',
+    'Morbid',
+    'Loathstone',
+    'Limestone',
+    'Mangrove',
+    'Maintain',
+    'Majestic',
+    'Optimal'
+]
+
+insanity = [
+    'Oncologist',
+    'Gynecologist',
+    'Neurologist',
+    'Nationalism',
+    'Chiaroscurist',
+    'Logorrhea',
+    'Disinterested',
+    'Disheartened',
+    'Supercalifragilisticexpialidocious',
+    'Worcestershire'
+]
+
+# Hangman visuals for the game
+
+
